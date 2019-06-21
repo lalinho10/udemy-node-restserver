f5470affe9b44af2cdf34be5607dce55208d5f61
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2019-06-21 13:22:38 -0500

    La conexión remota se actualiza como variable de ambiente de heroku

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let dbConnection;
 if (process.env.NODE_ENV === 'dev') {
     dbConnection = 'mongodb://localhost:27017/CafeDB';
 } else {
-    dbConnection = 'mongodb+srv://lalinho10:o7vnwLdDKc0Wr4iv@cluster0-mpl51.mongodb.net/CafeDB';
+    dbConnection = process.env.MONGO_DBCONNECTION;
 }
 
 process.env.DB_CONNECTION = dbConnection;
