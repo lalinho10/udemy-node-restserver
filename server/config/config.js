20fb7e2890141f55df621c3c29bb9abf81bf486c
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2019-06-21 12:41:02 -0500

    Corrección en el nombre de la BD

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let dbConnection;
 if (process.env.NODE_ENV === 'dev') {
     dbConnection = 'mongodb://localhost:27017/CafeDB';
 } else {
-    dbConnection = 'mongodb+srv://lalinho10:o7vnwLdDKc0Wr4iv@cluster0-mpl51.mongodb.net/test';
+    dbConnection = 'mongodb+srv://lalinho10:o7vnwLdDKc0Wr4iv@cluster0-mpl51.mongodb.net/CafeDB';
 }
 
 process.env.DB_CONNECTION = dbConnection;
